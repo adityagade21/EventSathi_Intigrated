cc8473f55c993f3ed99721e87366d5b4bcefbc26
adityagade21 <aditya@eventsathi.com>
2015-08-22 16:24:45 +0530

    test

---
+++ BL/BAFAQ.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PL;
+using DL;
+
+namespace BL
+{
+   public  class BAFAQ
+    {
+       DAFAQ objDA = new DAFAQ();
+       public int AddFAQ(PAAdminFAQ  objPA)
+       {
+           return objDA.AddFAQ(objPA);
+       }
+       public int DeleteFAQ(PAAdminFAQ objPA)
+       {
+           return objDA.DeleteFAQ(objPA);
+       }
+    }
+}
